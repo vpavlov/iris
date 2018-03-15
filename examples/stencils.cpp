f639b2cc330134ee5a2e78e443429631fc53dced
Valentin Pavlov <vpavlov@rila.bg>
2018-03-15 11:13:39 +0200

    remove laplacian3D_taylor reference from stencils example

--- examples/stencils.cpp
+++ examples/stencils.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <iris/first_derivative_taylor.h>
-#include <iris/laplacian3D_taylor.h>
 #include <iris/laplacian3D_pade.h>
 #include <iris/utils.h>
 
@@ -11,10 +10,6 @@ main()
     first_derivative_taylor d_dx(6, 0.01);
     d_dx.commit();
     d_dx.trace("d/dx");
-
-    // laplacian3D_taylor del(4, 0.01, 0.01, 0.01);
-    // del.commit();
-    // del.trace("Δ");
 
     laplacian3D_pade del2(0, 4, false, 1.0, 1.0, 1.0);
     del2.commit();
