2b999417ad01e52b46cc14ad3c68e9d4b526cbd8
Peicho Petkov <Peicho.Petkov@cern.ch>
2020-11-17 19:08:26 +0200

    poisson_solver gpu port

---
+++ src/poisson_solver_gpu.h
@@ -0,0 +1,54 @@
+// -*- c++ -*-
+//==============================================================================
+// IRIS - Long-range Interaction Solver Library
+//
+// Copyright (c) 2017-2019, the National Center for Supercomputing Applications
+//
+// Primary authors:
+//     Valentin Pavlov <vpavlov@rila.bg>
+//     Peicho Petkov <peicho@phys.uni-sofia.bg>
+//     Stoyan Markov <markov@acad.bg>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//==============================================================================
+#ifndef __IRIS_GPU_POISSON_SOLVER_H__
+#define __IRIS_GPU_POISSON_SOLVER_H__
+
+#include "state_accessor_gpu.h"
+
+namespace ORG_NCSA_IRIS {
+
+    class poisson_solver_gpu : protected state_accessor_gpu {
+
+    public:
+	poisson_solver_gpu(class iris_gpu *obj);
+	~poisson_solver_gpu();
+
+	virtual void commit() = 0;
+	virtual void solve() = 0;
+	virtual void handle_box_resize() = 0;
+
+	void set_dirty(bool in_dirty) { m_dirty = in_dirty; };
+
+    protected:
+	bool m_dirty;  // wether to recalculate on commit
+    };
+}
+
+#endif
